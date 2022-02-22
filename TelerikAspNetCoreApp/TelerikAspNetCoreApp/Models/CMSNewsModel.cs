efaabd18474ab83e0f7da9fda95671e74481f152
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2022-02-22 17:18:04 +0700

    update code

---
+++ TelerikAspNetCoreApp/TelerikAspNetCoreApp/Models/CMSNewsModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelerikAspNetCoreApp.Models
+{
+    public class CMSNewsModel
+    {
+
+        //[AllowHtml]
+        public string NewsContent
+        {
+            get;
+            set;
+        }
+
+        public int ID
+        {
+            get;
+            set;
+        }
+        public string LogoImageFile { get; set; }
+        public string LogoImage
+        {
+            get;
+            set;
+        }
+        public string DetailImageFile { get; set; }
+        public string DetailImage
+        {
+            get;
+            set;
+        }
+        public string CoverImageFile { get; set; }
+        public string CoverImage
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
