2a108c354c39e692155d5065631b338d6dd1c932
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2022-04-04 09:05:13 +0700

    update code

---
+++ OTPService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OTPService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
