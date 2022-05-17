7d6972a6ad8aecf8551cc9c8926f9a1971057f1d
huahoangnghinhthu <huahoangnghinhthu@ms-apptech.com>
2022-05-17 21:51:11 +0700

    update THISO UI

---
+++ FirebaseCloudMessaging/Models/ResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace FirebaseCloudMessaging.Models
+{
+    public class ResponseModel
+    {
+        [JsonProperty("isSuccess")]
+        public bool IsSuccess { get; set; }
+
+        [JsonProperty("message")]
+        public bool Message { get; set; }
+    }
+}
